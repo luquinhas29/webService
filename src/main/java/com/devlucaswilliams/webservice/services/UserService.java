e2e6d0ca562559875d69d910cb3e7076ac26787f
Lucas williams <lucaswilliamscesar@gmail.com>
2023-12-17 20:35:06 -0300

    User update

--- src/main/java/com/devlucaswilliams/webservice/services/UserService.java
+++ src/main/java/com/devlucaswilliams/webservice/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.Repository;
 import org.springframework.stereotype.Service;
 
 import com.devlucaswilliams.webservice.entities.User;
@@ -34,4 +35,16 @@ public class UserService {
 		userRepository.deleteById(id);
 	}
 
+	public User update(Long id, User obj) {
+		User entity = userRepository.getReferenceById(id);
+		updateData(entity, obj);
+		return userRepository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		
+	}
 }
