52d145f1639551c0d4beabfe20ea08173128b44c
Lucas williams <lucaswilliamscesar@gmail.com>
2023-12-19 08:44:53 -0300

    Exception handling - delete

--- src/main/java/com/devlucaswilliams/webservice/services/UserService.java
+++ src/main/java/com/devlucaswilliams/webservice/services/UserService.java
@@ -1,14 +1,16 @@
 package com.devlucaswilliams.webservice.services;
 
 import java.util.List;
-
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.devlucaswilliams.webservice.entities.User;
 import com.devlucaswilliams.webservice.repositories.UserRepository;
+import com.devlucaswilliams.webservice.services.exceptions.DataBaseException;
 import com.devlucaswilliams.webservice.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -33,8 +35,14 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		userRepository.deleteById(id);
+	}catch (EmptyResultDataAccessException e) {
+		throw new ResourceNotFoundException(id);
+	}catch(DataIntegrityViolationException e) {
+		throw new DataBaseException(e.getMessage());
 	}
+}
 
 	public User update(Long id, User obj) {
 		User entity = userRepository.getReferenceById(id);
